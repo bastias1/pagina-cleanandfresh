f3311f712bad6d01c0ce011ba44959735378379d
bastias <ayrtonbastias.a@gmail.com>
2024-11-16 13:00:19 -0300

    Revert "Merge branch 'master' of https://github.com/bastias1/pagina-cleanandfresh"
    This reverts commit 12e11de9a78591a8f76a5d6e9c2919dae32f2971, reversing
    changes made to e2bd462d58107a511b7be90c32e54c110e0c4628.

--- CleanAndFresh/urls.py
+++ CleanAndFresh/urls.py
@@ -20,19 +20,17 @@ from primeraApp import views
 #estas 2 lineas de abajo son las librerias para agregar imagenes
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #Inicio vista Cliente
     path('',views.index),
     path('servicios/',views.servicios),
+    path('login/',views.login),
     path('agendar/',views.agendar),
     #Fin vista cliente
 
     #Inicio vista Empleados/Admin
-<<<<<<< HEAD
     path('adminDashboard/', views.adminDashboard),
     path('gestion-horas-agendadas/',views.dashboardHorasAgendadas),
     path('creacion-empleados/',views.registro_empleados_view),
@@ -43,17 +41,6 @@ urlpatterns = [
     path('agregar-servicios/',views.agregarServicio),
     path('eliminar-servicio/<int:id>/', views.eliminarServicio, name='eliminarServicio'),
 
-=======
-     path('login/',auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
-    path('adminDashboard/', login_required(views.adminDashboard)),
-    path('gestion-horas-agendadas/',login_required(views.dashboardHorasAgendadas)),
-    path('creacion-empleados/',login_required(views.registro_empleados_view)),
-    path ('gestion-empleados/', login_required(views.gestionarEmpleados)),
-    path('eliminar-empleado/<int:id>',login_required(views.eliminarEmpleado)),
-    path('modificar-empleado/<int:id>',login_required(views.eliminarEmpleado)),
-    path('gestion-servicios/',login_required(views.gestionServicios),name='gestionServicios'),
-    path('agregar-servicios/',login_required(views.agregarServicio)),
->>>>>>> 7b1dec9eb56857a1c46c010dc8328fcb7f7d1448
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #lo que esta despues del ']' hace la magia con las imagenes
